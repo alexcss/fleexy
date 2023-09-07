26b3153f6222b7302ad1381cd83c90bb1d393533
Andrii Khan <iquach@icloud.com>
2023-09-07 09:29:38 +0200

    our team section

--- inc/Gutenberg/Blocks/Our_Team/Block.php
+++ inc/Gutenberg/Blocks/Our_Team/Block.php
@@ -13,12 +13,37 @@ class Block extends Gutenberg_Block {
 
 	public function init_fields(): void {
 
+		global $wpdb;
+
+		$query = "SELECT * FROM {$wpdb->prefix}bookingpress_staffmembers";
+
+		$results = $wpdb->get_results( $query, ARRAY_A );
+
+		$data = [];
+
+		if ( ! empty( $results ) ) {
+			foreach ( $results as $result ) {
+				$data[ $result['bookingpress_staffmember_id'] ] = $result['bookingpress_staffmember_firstname'] . ' ' . $result['bookingpress_staffmember_lastname'];
+			}
+		}
+
+
 		$this->add_field( [
 			'name'          => 'title',
 			'label'         => __( 'Title', 'fp' ),
 			'type'          => 'textarea',
 			'default_value' => 'Meet Our Team',
 			'instructions'  => __( 'if you want to highlight a word use this rule __TEXT__', 'fp' ),
+		] );
+
+		$this->add_field( [
+			'name'     => 'team',
+			'label'    => __( 'Team', 'fp' ),
+			'type'     => 'select',
+			'choices'  => $data,
+			'multiple' => 1,
+			'ui'       => 1,
+			'ajax'     => 1,
 		] );
 
 		$this->add_field( [
