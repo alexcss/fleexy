c9f85579ef742d2777a729338f81465a551ff9c2
alexcss <kalyuzhnyi@gmail.com>
2023-09-21 09:28:07 +0300

    Add Container block for text pages

---
+++ inc/Gutenberg/Blocks/Container/Block.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FP\Gutenberg\Blocks\Container;
+
+defined( 'ABSPATH' ) || exit;
+
+use FP\Gutenberg\Gutenberg_Block;
+
+class Block extends Gutenberg_Block {
+
+	// dashes only
+	const NAME = 'fp-container';
+
+	public function init_fields(): void {
+
+	}
+
+}
