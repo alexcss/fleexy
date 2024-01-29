49533610cd2d9c0429ed68818040409405c20429
alexcss <kalyuzhnyi@gmail.com>
2024-01-29 10:44:20 +0200

    Restore Booky widget

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -4,10 +4,10 @@ export class Main {
     this.initBookingLinks();
 	}
   removeBooksyWidget(){
-    const overlay = document.querySelector('.yWidgetCover');
+    const overlay = document.querySelector('.booksy-widget-overlay');
     if (overlay) overlay.remove();
 
-    const widget = document.querySelector('.yWidgetShow');
+    const widget = document.querySelector('.booksy-widget-dialog');
     if (widget) widget.remove();
   }
   initBookingLinks(){
@@ -16,24 +16,24 @@ export class Main {
 
     // bookingLinks.forEach(item => item.setAttribute('data-bs-toggle', 'offcanvas'));
     bookingLinks.forEach(item => item.addEventListener('click', () => {
-      const booksyBtn = document.querySelector('a.yButton.bottom');
+      const booksyBtn = document.querySelector('.booksy-widget-button');
       if (booksyBtn){
         booksyBtn.dispatchEvent(new Event('click'));
       }
     }));
 
-    // document.addEventListener('click', e => {
-    //   if (e.target.classList.contains('booksy-widget-overlay')) {
-    //     this.removeBooksyWidget()
-    //   }
-    // });
+    document.addEventListener('click', e => {
+      if (e.target.classList.contains('booksy-widget-overlay')) {
+        this.removeBooksyWidget()
+      }
+    });
 
-    // document.addEventListener('keydown', evt => {
-    //   evt = evt || window.event;
-    //   if (evt.keyCode == 27) {
-    //     this.removeBooksyWidget()
-    //   }
-    // });
+    document.addEventListener('keydown', evt => {
+      evt = evt || window.event;
+      if (evt.keyCode == 27) {
+        this.removeBooksyWidget()
+      }
+    });
 
   }
 }
